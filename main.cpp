5cf2ffd69c865e89dddf51bd951e3be42c743c00
k1r1ll <k1r1ll@k1r1ll.k1r1ll>
2025-11-08 13:25:31 +0300

    next

--- main.cpp
+++ main.cpp
@@ -1,74 +1,31 @@
 #include <iostream>
-#include <cstddef>
 
-size_t detsize(size_t n1, size_t n2, size_t m1, size_t m2, size_t i)
+int det(const int * a, size_t n)
 {
-    size_t minn = n1 > n2 ? n2 : n1;
-    size_t minm = m1 > m2 ? m2 : m1;
-    if (i < minn)
+    if (!n)
     {
-        return m1 + m2;
+        return 0;
+    }
+    else if(n == 1)
+    {
+        return *a;
+    }
+    else if(n == 2)
+    {
+        return a[0]*a[3] - a[1]*a[2];
     }
     else
     {
-        return n1 > n2 ? m1 : m2;
+        //для каждого эл-та 1-ой строки
+        //опорный эл-т == эл-т
+        //сделать матрицу для минора - new!!!
+        //заполнить минор
+        //считаем определитель минорами рекурсивно - det new
+        //накапливаем определитель
     }
 }
 
-int** ct(size_t n1, size_t m1, size_t n2, size_t m2)
+int main()
 {
-    size_t n = n1 > n2 ? n1 : n2;
-    int ** c = new int*[n];
-    size_t i = 0;
-    try 
-    {
-        for (; i < n; ++i)
-        {
-            size_t m = detsize(n1, n2, m1, m2, i);
-            c[i] = new int[m];
-        }
-    }
-    catch (...)
-    {
-        rm(c, i);
-        throw;
-    }
-}
 
-int* copy(const int * v, size_t k, int * w)
-{
-    for (size_t i = 0; i < k; ++i)
-    {
-        w[i] = v[i];
-    }
-    return w + k;
-}
-
-void rm(int** a, size_t k)
-{
-    for (size_t i = 0; i < k; ++i)
-    {
-        delete [] a[i];
-    }
-    delete [] a;
-}
-
-int** conced_rows(const int * const * a, size_t n1, size_t m1, const int * const * b, size_t n2, size_t m2)
-{
-    size_t n = n1 > n2 ? n2 : n1;
-    int ** c = ct(n1, n2, m1, m2);
-    for (size_t i = 0; i < n; ++i)
-    {
-        const int * rowa = a[i];
-        const int * rowb = b[i];
-        int* rowc = c[i];
-        rowc = copy(rowa, m1, rowc);
-        rowc = copy(rowb, m2, rowc);
-    }
-
-
-
-
-
-    return c;
 }
